c7fd8b4c54cb49517e3e29b4d0dfddfbbfb44ebc
Andi <38726990+andismood@users.noreply.github.com>
2024-07-24 13:59:47 +0700

    Update DashboardAdminController.php

--- app/Http/Controllers/DashboardAdminController.php
+++ app/Http/Controllers/DashboardAdminController.php
@@ -12,6 +12,7 @@ use PhpParser\Node\Stmt\TryCatch;
 class DashboardAdminController extends Controller
 {
     public function index(){
+        $test = "";
         $menu = Menu::getAllMenu();
         return view('admin.index', ['data' => $menu]);
     }
